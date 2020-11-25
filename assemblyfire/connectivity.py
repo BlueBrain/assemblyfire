378e91dde1d28616162dd791dc071571ef67fb79
Michael Reimann <michael@reimann.science>
2020-11-25 11:50:55 +0100

    Implemented ConnectivityMatrix input and output

--- assemblyfire/connectivity.py
+++ assemblyfire/connectivity.py
@@ -1,6 +1,7 @@
 import numpy
 import pandas
-from ..assemblies import Assembly
+import h5py
+from .assemblies import Assembly
 
 
 class ConnectivityMatrix(object):
@@ -15,13 +16,25 @@ class ConnectivityMatrix(object):
     def __make_lookup__(self):
         return pandas.Series(numpy.arange(len(self._gids)), index=self._gids)
 
+    @property
+    def matrix(self):
+        return self._m
+
+    @property
+    def dense_matrix(self):
+        return self._m.todense()
+
+    @property
+    def array(self):
+        return numpy.array(self.dense_matrix)
+
     @staticmethod
     def __extract_gids__(an_obj):
         if isinstance(an_obj, Assembly):
             return an_obj.gids
         return an_obj
 
-    def submat(self, sub_gids, sub_gids_post=None):
+    def submatrix(self, sub_gids, sub_gids_post=None):
         """
         Return a submatrix
         :param sub_gids: Subpopulation to get the submatrix for. Can be either a list of gids, or an Assembly object
@@ -35,11 +48,59 @@ class ConnectivityMatrix(object):
         idx = self._lookup[self.__extract_gids__(sub_gids)]
         return self._m[numpy.ix_(idx, idx)]
 
-    @classmethod
-    def from_bluepy(cls, cfg, gids):
-        raise NotImplemented()
+    def dense_submatrix(self, sub_gids, sub_gids_post=None):
+        return self.submatrix(sub_gids, sub_gids_post=sub_gids_post).todense()
+
+    def subarray(self, sub_gids, sub_gids_post=None):
+        return numpy.array(self.dense_submatrix(sub_gids, sub_gids_post=sub_gids_post))
 
     @classmethod
-    def from_h5(cls, fn, gids):
-        raise NotImplemented()
+    def from_bluepy(cls, cfg, gids=None):
+        import bluepy.v2 as bluepy
+        from tqdm import tqdm
+        from scipy import sparse
+        sim = bluepy.Simulation(cfg)
+        if gids is None:
+            from .utils import get_E_gids
+            gids = get_E_gids(sim.circuit, sim.target)
+
+        conv = pandas.Series(numpy.arange(len(gids)), index=gids)
+        indptr = [0]
+        indices = []
+
+        for gid in tqdm(gids, desc="Building connection matrix"):
+            aff = conv[numpy.intersect1d(sim.circuit.connectome.afferent_gids(gid), gids)]
+            indices.extend(aff)
+            indptr.append(len(indices))
+        data = numpy.ones_like(indices, dtype=bool)
+        adj_mat = sparse.csc_matrix((data, indices, indptr), shape=(len(gids), len(gids)))
+        return cls(adj_mat, gids)
+
+    @classmethod
+    def from_h5(cls, fn, group_name, prefix=None):
+        from scipy import sparse
+        if prefix is None:
+            prefix = "connectivity"  # TODO: see below
+
+        with h5py.File(fn, 'r') as h5:
+            prefix_grp = h5[prefix]
+            data_grp = prefix_grp[group_name]
+            data = data_grp["data"][:]
+            indices = data_grp["indices"][:]
+            indptr = data_grp["indptr"][:]
+            gids = data_grp["gids"][:]
+            adj_mat = sparse.csc_matrix((data, indices, indptr), shape=(len(gids), len(gids)))
+            return cls(adj_mat, gids)
+
+    def to_h5(self, fn, prefix=None):
+        if prefix is None:
+            prefix = "connectivity"  # TODO: not overly hard coded
+
+        with h5py.File(fn, "a") as h5:
+            prefix_grp = h5.require_group(prefix)
+            data_grp = prefix_grp.create_group("matrix")  # TODO: not overly hard coded
+            data_grp.create_dataset("data", data=self._m.data)
+            data_grp.create_dataset("indices", data=self._m.indices)
+            data_grp.create_dataset("indptr", data=self._m.indptr)
+            data_grp.create_dataset("gids", data=self._gids)
 
