0ef798d28d66f4a304459b23338f6106c3699eb5
Ecker András <andras.ecker@epfl.ch>
2020-11-25 12:58:06 +0100

    cli docs updated

--- assemblyfire/cli.py
+++ assemblyfire/cli.py
@@ -36,6 +36,6 @@ def find_assemblies(config_path):
 @cli.command()
 @click.argument("config_path", required=True)
 def consensus(config_path):
-    """CLI for `find_assemblies.py/run()`"""
+    """CLI for `find_consensus_assemblies.py/run()`"""
     from assemblyfire.find_consensus_assemblies import run
     run(config_path)
