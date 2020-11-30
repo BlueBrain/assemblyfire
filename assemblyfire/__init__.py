e7fe0309cc43dc725457c03d8ff323f256a1f86c
Ecker András <andras.ecker@epfl.ch>
2020-11-30 17:15:55 +0100

    order updated

--- assemblyfire/__init__.py
+++ assemblyfire/__init__.py
@@ -4,8 +4,8 @@ from assemblyfire.version import __version__
 from assemblyfire.spikes import SpikeMatrixGroup
 from assemblyfire.assemblies import AssemblyProjectMetadata, Assembly, AssemblyGroup, ConsensusAssembly
 from assemblyfire.connectivity import ConnectivityMatrix
+from assemblyfire.topology import NetworkAssembly
 from assemblyfire.clustering import cluster_spikes, detect_assemblies, cluster_assemblies
 from assemblyfire import utils
 from assemblyfire import plots
-from assemblyfire.topology import NetworkAssembly
 
