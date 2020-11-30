de67df48e1fe885400b64ae211a66751a333d4b8
Egas Santander Daniela <daniela.egassantander@epfl.ch>
2020-11-30 20:33:09 +0100

    Added k-core filtration computation.  This class is currently not working because ConnectivityMatrix has changed.

--- assemblyfire/topology.py
+++ assemblyfire/topology.py
@@ -1,24 +1,32 @@
 from assemblyfire.connectivity import ConnectivityMatrix
 
-
+#Currently not working because ConnectivityMatrix has changed!
 class NetworkAssembly(ConnectivityMatrix):
     """
     A class derived from ConnectivityMatrix with additional information on networks metrics of the subgraph associated to an assembly within the connectivity matrix of the circuit.
     """
     
     
-    def simplex_counts(self):
+    def simplex_counts(self,sub_gids):
         """Return a the simplex counts of submatrix specified by `sub_gids`"""
         import pyflagser
         sub_mat= submatrix(self,sub_gids)
         return pyflagser.flagser_count_unweighted(sub_mat, directed=True)
         
 
-    def betti_counts(self):
+    def betti_counts(self,sub_gids):
         """Return a the betti counts of submatrix specified by `sub_gids`"""
         import pyflagser
         sub_mat= submatrix(self,sub_gids)
         return pyflagser.flagser_unweighted(sub_mat, directed=True)
+        
+    def core_number(self,sub_gids):
+        """Returns k core of directed graph, where degree of a vertex is the sum of in degree and out degree"""
+        #TODO: Implement directed (k,l) core and k-core of underlying undirected graph (very similar to this)
+        import networkx
+        G = networkx.from_numpy_matrix(self)
+        return networkx.algorithms.core.core_number(G) #Very inefficient (returns a dictionary!).  Look for different implementation
+        
     
     
     #TODO: Simplex counts associated to a dictionary of assembly groups e.g. consensus assembly
