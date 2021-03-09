8fd2071fb8cd34903f06e70ceb0be0a9c539f5fd
Ecker András <andras.ecker@epfl.ch>
2021-03-09 19:51:01 +0100

    more plots added

--- use_cases/consensus.py
+++ use_cases/consensus.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Exemplar script which calculates simplices in consensus assemblies for this specific use cases
+Exemplar script which gets consensus assemblies for this specific use cases at plots several basic stuff for them
 (At this point everything should be saved in the general assemblyfire format, so nothing fancy/extra here)
 last modified: András Ecker 02.2021
 """
@@ -11,8 +11,9 @@ import numpy as np
 from assemblyfire.utils import load_assemblies_from_h5, load_consensus_assemblies_from_h5
 from assemblyfire.assemblies import consensus_over_seeds_hc
 from assemblyfire.topology import NetworkAssembly, simplex_counts_consensus
-from assemblyfire.plots import plot_simplex_counts_consensus, plot_cons_cluster_seqs
-from find_assemblies import load_patterns
+from assemblyfire.plots import plot_simplex_counts_consensus, plot_cons_cluster_seqs,\
+                               plot_pattern_cons_clusters, plot_consensus_mtypes
+from find_assemblies import load_patterns, map_gids_to_depth, get_layer_boundaries
 
 data_dir = "/gpfs/bbp.cscs.ch/project/proj96/home/ecker/assemblyfire/use_cases/input_data"
 fig_path = "/gpfs/bbp.cscs.ch/project/proj96/home/ecker/figures/toposample"
@@ -34,15 +35,24 @@ def _find_assembly(consensus_assembly_dict, seed, assembly_id):
 
 
 def consensus_assembly_cluster_seq(consensus_assembly_dict, assembly_metadata, data_dir):
-    """Recolor cluster seqs. based on consensus assemblies"""
+    """Recolor cluster seqs. based on consensus assemblies
+    and plots grand avg. per pattern overview of consensus assemblies"""
 
+    # store results for grand avg. plot
+    cons_clusters_dict = {}
+    t_bins_dict = {}
+    stim_times_dict = {}
+    patterns_dict = {}
     # the beginning is just duplicated from `use_cases/find_assemblies.py` ...
     stim_times, patterns = load_patterns(data_dir)
     t_full = stim_times[-1] + 200
     t_slices = np.arange(0, t_full+30000, 30000)
     for i, (t_start, t_end) in enumerate(zip(t_slices[:-1], t_slices[1:])):
+        key = "seed%i" % i
         t_bins = np.arange(t_start, t_end + 20., 20.)  # bin size = 20 ms from the rest of the use case
         idx = np.where((t_start <= stim_times) & (stim_times < t_end))[0]
+        stim_times_dict[key] = stim_times[idx]
+        patterns_dict[key] = patterns[idx]
 
         # from here it's not that hacky as it's just accessing saved files...
         orig_clusters = assembly_metadata["seed%i" % i]["clusters"]
@@ -54,9 +64,34 @@ def consensus_assembly_cluster_seq(consensus_assembly_dict, assembly_metadata, d
             t_bins = t_bins[:len(cons_clusters)]  # the last slice is shorter
         if cons_clusters.shape[0] == 1499:
             cons_clusters = np.insert(cons_clusters, 1499, -1)  # I have no clue why this happens...
+        t_bins_dict[key] = t_bins
+        cons_clusters_dict[key] = cons_clusters
+
         fig_name = os.path.join(fig_path, "cons_cluster_seq_seed%i.png" % i)
         plot_cons_cluster_seqs(cons_clusters, t_bins, stim_times[idx], patterns[idx],
                                n_clusters=len(consensus_assembly_dict.keys()), fig_name=fig_name)
+    fig_name = os.path.join(fig_path, "cons_clusters_patterns.png")
+    plot_pattern_cons_clusters(cons_clusters_dict, t_bins_dict, stim_times_dict, patterns_dict,
+                               n_clusters=len(consensus_assembly_dict.keys()), fig_name=fig_name)
+
+
+def consensus_depth_profile(consensus_assembly_dict, network, data_dir):
+    """Plots consensus assembly depth profile and mtype composition"""
+
+    depths = map_gids_to_depth(data_dir)
+    ystuff = get_layer_boundaries(data_dir)
+
+    all_gids = network.gids
+    consensus_gids = [assembly.gids for _, assembly in consensus_assembly_dict.items()]
+    union_gids = [assembly.union.gids for _, assembly in consensus_assembly_dict.items()]
+
+    mtypes = network.mtypes  # this is a bit weird that it's stored in NetworkAssembly...
+    consensus_mtypes = [mtypes[np.searchsorted(all_gids, gids)] for gids in consensus_gids]
+    union_mtypes = [mtypes[np.searchsorted(all_gids, gids)] for gids in union_gids]
+
+    fig_name = os.path.join(fig_path, "consensus_mtypes.png")
+    plot_consensus_mtypes(union_gids, union_mtypes, consensus_gids, all_gids, consensus_mtypes, mtypes,
+                          ystuff, depths, fig_name)
 
 
 if __name__ == "__main__":
@@ -77,7 +112,9 @@ if __name__ == "__main__":
 
     network = NetworkAssembly.from_h5(h5f_name, group_name="full_matrix", prefix="connectivity")
     simplex_counts, simplex_counts_control = simplex_counts_consensus(consensus_assembly_dict, network, N=1)
-    fig_name = os.path.join(fig_path, "simplex_counts_consensus.png")
+    fig_name = os.path.join(fig_path, "consensus_simplex_counts.png")
     plot_simplex_counts_consensus(simplex_counts, simplex_counts_control, fig_name)
+
+    consensus_depth_profile(consensus_assembly_dict, network, data_dir)
 
 
