6e4568c57029a93897be5a67d2dcf0322b8370f4
Ecker András <andras.ecker@epfl.ch>
2022-01-13 11:36:03 +0100

    moved connectivity.py to the conntility package

--- assemblyfire/assemblyfire/__init__.py
+++ assemblyfire/assemblyfire/__init__.py
@@ -3,10 +3,8 @@
 from assemblyfire.version import __version__
 from assemblyfire.spikes import SpikeMatrixGroup, spikes2mat, sign_rate_std, spikes_to_h5
 from assemblyfire.assemblies import Assembly, AssemblyGroup, ConsensusAssembly
-from assemblyfire.connectivity import ConnectivityMatrix
 from assemblyfire.topology import NetworkAssembly
 from assemblyfire.clustering import cluster_sim_mat, cluster_spikes, detect_assemblies, cluster_assemblies
 from assemblyfire import utils
 from assemblyfire import plots
-from .topological_analysis import TopologicalAnalysis
 
