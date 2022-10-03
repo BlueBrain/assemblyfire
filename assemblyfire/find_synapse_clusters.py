024d237a535b1f083cc4b0e2fe357d772966bc75
Ecker András <andras.ecker@epfl.ch>
2022-10-03 13:58:48 +0200

    loading lookup df made more general

--- assemblyfire/find_synapse_clusters.py
+++ assemblyfire/find_synapse_clusters.py
@@ -47,47 +47,48 @@ def run(config_path, debug):
 
     config = Config(config_path)
     L.info(" Load in assemblies and connectivity matrix from %s" % config.h5f_name)
-    target_range, min_nsyns = config.syn_clustering_target_range, config.syn_clustering_min_nsyns
-    mtypes, n_samples = config.syn_clustering_mtypes, config.syn_clustering_n_neurons_sample
     sim_paths = utils.get_sim_path(config.root_path)
+    c = utils.get_bluepy_cicruit(sim_paths.iloc[0])
+    loc_df = utils.get_loc_df(config.syn_clustering_lookup_df_pklfname, c, config.target, config.syn_clustering_target)
+    target_gids = loc_df["post_gid"].unique()
     assembly_grp_dict, _ = utils.load_assemblies_from_h5(config.h5f_name, config.h5_prefix_assemblies)
-    syn_df = utils.load_pkl_df(config.syn_clustering_lookup_df_pklfname)
     conn_mat = AssemblyTopology.from_h5(config.h5f_name,
                                         prefix=config.h5_prefix_connectivity, group_name="full_matrix")
-    L.info(" Detecting synapse clusters and saving them to files")
+    target_range, min_nsyns = config.syn_clustering_target_range, config.syn_clustering_min_nsyns
+    mtypes, n_samples = config.syn_clustering_mtypes, config.syn_clustering_n_neurons_sample
 
+    L.info(" Detecting synapse clusters and saving them to files")
     for seed, assembly_grp in tqdm(assembly_grp_dict.items(), desc="Iterating over seeds"):
         cluster_dfs = {}
-        sim = utils.get_bluepy_simulation(sim_paths.loc[int(seed.split("seed")[1])])
-        target_gids = sim.circuit.cells.ids({"$target": config.syn_clustering_target})
         for assembly in tqdm(assembly_grp.assemblies, desc="%s syn. clusters" % seed, leave=False):
-            gids = _get_degree_sorted_assembly_gids_within_target(sim.circuit, target_gids, conn_mat,
+            gids = _get_degree_sorted_assembly_gids_within_target(c, target_gids, conn_mat,
                                                                   assembly, mtypes, n_samples)
-            syn_df_gids = syn_df.loc[syn_df["post_gid"].isin(gids)]
+            loc_df_gids = loc_df.loc[loc_df["post_gid"].isin(gids)]
             # create a fake assembly "group" in order to look for *within* assembly clusters only
             single_assembly_grp = AssemblyGroup([assembly], all_gids=assembly.gids)
             # get clusters and save them to pickle
             if debug:
                 fig_dir = os.path.join(config.fig_path, "%s_debug" % seed)
                 utils.ensure_dir(fig_dir)
-                cluster_df = cluster_synapses(syn_df_gids, single_assembly_grp, target_range, min_nsyns,
-                                              fig_dir=fig_dir, base_assembly_idx=assembly.idx[0], c=sim.circuit)
+                cluster_df = cluster_synapses(loc_df_gids, single_assembly_grp, target_range, min_nsyns,
+                                              fig_dir=fig_dir, base_assembly_idx=assembly.idx[0], c=c)
             else:
-                cluster_df = cluster_synapses(syn_df_gids, single_assembly_grp, target_range, min_nsyns)
+                cluster_df = cluster_synapses(loc_df_gids, single_assembly_grp, target_range, min_nsyns)
             utils.save_syn_clusters(config.syn_clustering_save_dir, assembly.idx, cluster_df)
+
+            sim = utils.get_bluepy_simulation(sim_paths.loc[int(seed.split("seed")[1])])
             if assembly.idx[0] == 0:  # late assembly TODO: not hard code this distinction
                 gids = _get_rate_sorted_assembly_gids_within_target(sim, target_gids, assembly.gids, mtypes,
                                                                     n_samples, config.t_start, config.t_end)
-                syn_df_gids = syn_df.loc[syn_df["post_gid"].isin(gids)]
+                loc_df_gids = loc_df.loc[loc_df["post_gid"].isin(gids)]
                 if debug:
-                    late_cluster_df = cluster_synapses(syn_df_gids, assembly_grp, target_range, min_nsyns,
-                                                       fig_dir=fig_dir, base_assembly_idx=assembly.idx[0], c=sim.circuit)
+                    late_cluster_df = cluster_synapses(loc_df_gids, assembly_grp, target_range, min_nsyns,
+                                                       fig_dir=fig_dir, base_assembly_idx=assembly.idx[0], c=c)
                 else:
-                    late_cluster_df = cluster_synapses(syn_df_gids, assembly_grp, target_range, min_nsyns)
+                    late_cluster_df = cluster_synapses(loc_df_gids, assembly_grp, target_range, min_nsyns)
                 utils.save_syn_clusters(config.syn_clustering_save_dir, assembly.idx, late_cluster_df, late_assembly=True)
             # some extra plotting
-            cluster_df["rho"] = utils.get_syn_properties(sim.circuit, cluster_df.index.to_numpy(),
-                                                         ["rho0_GB"])["rho0_GB"]
+            cluster_df["rho"] = utils.get_syn_properties(c, cluster_df.index.to_numpy(), ["rho0_GB"])["rho0_GB"]
             cluster_dfs[assembly.idx[0]] = cluster_df
         fig_name = os.path.join(config.fig_path, "rho0_syn_clusts_%s.png" % seed)
         plot_cond_rhos(cluster_dfs, fig_name)
