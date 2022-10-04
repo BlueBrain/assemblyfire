7dc79438a87f6214599c23952b472ef300351bec
Ecker András <andras.ecker@epfl.ch>
2022-10-04 14:25:25 +0200

    minor fix in load config

--- assemblyfire/get_connectivity_matrix.py
+++ assemblyfire/get_connectivity_matrix.py
@@ -20,7 +20,7 @@ def run(config_path):
     config = Config(config_path)
     L.info("Loading in connectivity matrix and saving it to: %s" % config.h5f_name)
     c = get_bluepy_circuit(get_sim_path(config.root_path).iloc[0])
-    load_cfg = {"loading": {"base_target": config.target, "properties": ["x", "y", "z", "mtype", "synapse_class"],
+    load_cfg = {"loading": {"base_target": config.target, "properties": ["x", "y", "z", "mtype"],
                             "atlas": [{"data": "[PH]y", "properties": ["[PH]y"]}]}}
     connectivity_matrix = ConnectivityMatrix.from_bluepy(c, load_cfg)
     connectivity_matrix.to_h5(config.h5f_name, prefix=config.h5_prefix_connectivity, group_name="full_matrix")
