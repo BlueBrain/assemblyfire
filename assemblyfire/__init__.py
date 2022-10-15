60161d9215edd9a5a7989a36f01ab5555d752d6c
Ecker András <andras.ecker@epfl.ch>
2022-10-15 12:50:38 +0200

    functions taken out

--- assemblyfire/__init__.py
+++ assemblyfire/__init__.py
@@ -1,7 +1,7 @@
 """assemblyfire"""
 
 from assemblyfire.version import __version__
-from assemblyfire.spikes import SpikeMatrixGroup, spikes2mat, sign_rate_std, spikes_to_h5
+from assemblyfire.spikes import SpikeMatrixGroup, spikes2mat
 from assemblyfire.assemblies import Assembly, AssemblyGroup, ConsensusAssembly
 from assemblyfire.topology import AssemblyTopology
 from assemblyfire.clustering import cluster_sim_mat, cluster_spikes, detect_assemblies, cluster_assemblies, cluster_synapses
