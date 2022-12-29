0f1f5ca51a3a563db2c6cb15b54e05f5ff04b460
Ecker András <andras.ecker@epfl.ch>
2022-12-29 14:53:06 +0100

    function renamed

--- assemblyfire/find_consensus_assemblies.py
+++ assemblyfire/find_consensus_assemblies.py
@@ -7,7 +7,7 @@ import logging
 
 from assemblyfire.config import Config
 from assemblyfire.utils import load_assemblies_from_h5
-from assemblyfire.assemblies import consensus_over_seeds_hc
+from assemblyfire.assemblies import consensus_over_seeds
 
 L = logging.getLogger("assemblyfire")
 
@@ -17,13 +17,11 @@ def run(config_path):
     Loads in project related info from yaml config file, finds consensus assemblies and saves them to h5
     :param config_path: str - path to project config file
     """
-
     config = Config(config_path)
     L.info(" Load in assemblies from %s" % config.h5f_name)
     assembly_grp_dict, _ = load_assemblies_from_h5(config.h5f_name, config.h5_prefix_assemblies)
-
     L.info(" Creating consensus assemblies and saving them to the same file")
-    consensus_over_seeds_hc(assembly_grp_dict, config.h5f_name, config.h5_prefix_consensus_assemblies, config.fig_path)
+    consensus_over_seeds(assembly_grp_dict, config.h5f_name, config.h5_prefix_consensus_assemblies, config.fig_path)
 
 
 
