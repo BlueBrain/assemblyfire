943d318292b26889e4de8bbe41f41dec92134506
Ecker András <andras.ecker@epfl.ch>
2023-01-15 21:31:44 +0100

    overwrite seed dict passed properly

--- assemblyfire/find_consensus_assemblies.py
+++ assemblyfire/find_consensus_assemblies.py
@@ -26,7 +26,7 @@ def run(config_path):
 
     L.info(" Load in spikes from %s, average them and detect average assemblies " % spikes.root_path)
     spike_matrix_dict, project_metadata = spikes.get_mean_sign_spike_matrix()
-    clusters_dict = cluster_spikes(spike_matrix_dict, {}, project_metadata, spikes.fig_path)
+    clusters_dict = cluster_spikes(spike_matrix_dict, spikes.overwrite_seeds, project_metadata, spikes.fig_path)
     nrn_loc_df = get_neuron_locs(get_sim_path(spikes.root_path).iloc[0], spikes.target)
     detect_assemblies(spike_matrix_dict, clusters_dict, spikes.core_cell_th_pct, spikes.h5f_name,
                       spikes.h5_prefix_avg_assemblies, nrn_loc_df, spikes.fig_path)
