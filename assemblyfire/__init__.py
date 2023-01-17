c6e15347e741f40638b30a9e8cd8fda2bf69b49d
Ecker András <andras.ecker@epfl.ch>
2023-01-17 16:00:54 +0100

    moved around Michael's functions and added CLI option for synapse NNDs

--- assemblyfire/__init__.py
+++ assemblyfire/__init__.py
@@ -4,7 +4,9 @@ from assemblyfire.version import __version__
 from assemblyfire.spikes import SpikeMatrixGroup, spikes2mat
 from assemblyfire.assemblies import Assembly, AssemblyGroup, ConsensusAssembly
 from assemblyfire.topology import AssemblyTopology
-from assemblyfire.clustering import cluster_sim_mat, cluster_spikes, detect_assemblies, cluster_assemblies, cluster_synapses
+from assemblyfire.syn_nnd import SynNNDResults
+from assemblyfire.clustering import cluster_sim_mat, cluster_spikes, detect_assemblies, cluster_assemblies,\
+                                    syn_nearest_neighbour_distances, cluster_synapses
 from assemblyfire import utils
 from assemblyfire import plots
 
