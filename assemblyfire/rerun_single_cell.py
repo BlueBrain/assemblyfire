bae76e2f5f63a5ff9fcafce619fa3c03b8abe61d
Ecker András <andras.ecker@epfl.ch>
2023-01-20 22:24:07 +0100

    spikes returned as DF

--- assemblyfire/rerun_single_cell.py
+++ assemblyfire/rerun_single_cell.py
@@ -1,8 +1,11 @@
 """
-TODO
+Rerun single cells in BGLibPy (for extra reporting and access to parameters to be modified)
+authors: András Ecker, Sirio Bolaños Puchet; last update: 01.2023
 """
 
 import os
+import gc
+import time
 import logging
 import numpy as np
 import pandas as pd
@@ -27,7 +30,7 @@ def get_gid_instantiation_vars(ssim):
 
 
 def run_sim(ssim, gid, pre_gids, pre_spike_trains, spike_loc):
-    """TODO"""
+    """Reruns simulation of a single `gid` w/ all the inputs from the network simulation"""
     # instantiate gid with replay on all synapses and the same input as it gets in the network simulation
     ssim.instantiate_gids([gid], add_synapses=True, add_projections=True, add_minis=True, intersect_pre_gids=pre_gids,
                           add_stimuli=True, add_replay=True, pre_spike_trains=pre_spike_trains)
@@ -44,8 +47,10 @@ def run_sim(ssim, gid, pre_gids, pre_spike_trains, spike_loc):
 
     ssim.run()
 
+    # return spike times as DF (more columns to be added later...)
     spike_times = np.array(spike_vec)
-    # get voltage recordings from all sections
+    spikes = pd.DataFrame(data=spike_times[spike_times > 0], columns=["spike_times"])
+    # get voltage recordings from all sections and create DF
     data, columns = [], []
     for section in all_sections:
         columns.append(section.name().split(".")[1])
@@ -53,23 +58,58 @@ def run_sim(ssim, gid, pre_gids, pre_spike_trains, spike_loc):
     vs = pd.DataFrame(data=np.hstack(data), columns=columns, index=cell.get_time())
     vs.index.name = "time"
 
-    return spike_times[spike_times > 0], vs
+    return spikes, vs
 
 
-def main(config_path, seed, gid=8473):
-    """TODO"""
+def run(config_path, seed, gid):
+    """Sets up and reruns single cell BGLibPy with 3 conditions: baseline, passive dendrites, and NMDA blocked,
+    and saves spikes and voltages of all sections from them"""
 
     config = Config(config_path)
-    save_dir = os.path.join(config.root_path, "analyses", "allsec_voltages")
+    save_dir = os.path.join(config.root_path, "analyses", "rerun_results")
     utils.ensure_dir(save_dir)
     sim_path = utils.get_sim_path(config.root_path).loc[seed]
-    L.info(" Instantiating %i from %s in BGLibPy" % (gid, sim_path))
-    ssim = utils.get_bglibpy_ssim(sim_path)
-    pre_gids, pre_spike_trains, spike_loc = get_gid_instantiation_vars(ssim)
+    L.info(" Instantiating %i from %s in BGLibPy " % (gid, sim_path))
 
     L.info(" Running sim w/ baseline conditions ")
-    spike_times, vs = run_sim(ssim, 8473, pre_gids, pre_spike_trains, spike_loc)
+    t1 = time.time()
+    ssim = utils.get_bglibpy_ssim(sim_path)
+    pre_gids, pre_spike_trains, spike_loc = get_gid_instantiation_vars(ssim)
+    spikes, vs = run_sim(ssim, gid, pre_gids, pre_spike_trains, spike_loc)
+    spikes["condition"] = "baseline"
+    spikes.to_pickle(os.path.join(save_dir, "seed%s_a%i_spikes.pkl" % (seed, gid)))
     vs.to_pickle(os.path.join(save_dir, "seed%s_a%i_baseline_voltages.pkl" % (seed, gid)))
     ssim.delete()
+    gc.collect()
+    t2 = time.time()
+    L.info(" Baseline sim. finished in: %s " % time.strftime("%H:%M:%S", time.gmtime(t2 - t1)))
+
+    '''TODO: add flags to `run_sim()` to run w/ different conditions
+    L.info(" Running sim w/ passive dendrites ")
+    ssim = utils.get_bglibpy_ssim(sim_path)
+    spikes_, vs = run_sim(ssim, 8473, pre_gids, pre_spike_trains, spike_loc)
+    spikes_["condition"] = "passive_dendrites"
+    spikes = pd.concat([spikes, spikes_], ignore_index=True)
+    spikes = spikes.sort_values("spike_times")
+    spikes.to_pickle(os.path.join(save_dir, "seed%s_a%i_spikes.pkl" % (seed, gid)))
+    vs.to_pickle(os.path.join(save_dir, "seed%s_a%i_passivedend_voltages.pkl" % (seed, gid)))
+    ssim.delete()
+    gc.collect()
+    t3 = time.time()
+    L.info(" Sim. w/ passive dendrites finished in: %s " % time.strftime("%H:%M:%S", time.gmtime(t3 - t2)))
+
+    L.info(" Running sim w/ NMDA channels blocked ")
+    ssim = utils.get_bglibpy_ssim(sim_path)
+    spikes_, vs = run_sim(ssim, 8473, pre_gids, pre_spike_trains, spike_loc)
+    spikes_["condition"] = "no_NMDA"
+    spikes = pd.concat([spikes, spikes_], ignore_index=True)
+    spikes = spikes.sort_values("spike_times")
+    spikes.to_pickle(os.path.join(save_dir, "seed%s_a%i_spikes.pkl" % (seed, gid)))
+    vs.to_pickle(os.path.join(save_dir, "seed%s_a%i_noNMDA_voltages.pkl" % (seed, gid)))
+    ssim.delete()
+    gc.collect()
+    t4 = time.time()
+    L.info(" Sim. w/ blocked NMDA channels finished in: %s " % time.strftime("%H:%M:%S", time.gmtime(t4 - t3)))
+    '''
 
 
