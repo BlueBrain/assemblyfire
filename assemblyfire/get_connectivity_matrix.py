a0e42c3e6eaeafd3f959e59e548de006117d7d03
Ecker András <andras.ecker@epfl.ch>
2023-06-12 18:12:34 +0200

    updated after replacing bluepy with bluepysnap

--- assemblyfire/get_connectivity_matrix.py
+++ assemblyfire/get_connectivity_matrix.py
@@ -22,6 +22,7 @@ def run(config_path):
     c = get_bluepy_circuit_from_root_path(config.root_path)
     load_cfg = {"loading": {"base_target": config.target, "properties": ["layer", "x", "y", "z", "mtype",
                                                                          "ss_flat_x", "ss_flat_y", "depth"]}}
-    connectivity_matrix = ConnectivityMatrix.from_bluepy(c, load_cfg)
-    connectivity_matrix.to_h5(config.h5f_name, prefix=config.h5_prefix_connectivity, group_name="full_matrix")
+    conn_mat = ConnectivityMatrix.from_bluepy(c, load_cfg, load_full=True,
+                                              connectome="S1nonbarrel_neurons__S1nonbarrel_neurons__chemical")
+    conn_mat.to_h5(config.h5f_name, prefix=config.h5_prefix_connectivity, group_name="full_matrix")
 
