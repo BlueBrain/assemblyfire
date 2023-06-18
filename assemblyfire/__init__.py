0c6c985dd368e33657deef3c2c49657e3d4b78eb
Ecker András <andras.ecker@epfl.ch>
2023-06-18 16:29:10 +0200

    stripped init

--- assemblyfire/__init__.py
+++ assemblyfire/__init__.py
@@ -1,12 +1,8 @@
 """assemblyfire"""
 
 from assemblyfire.version import __version__
-from assemblyfire.spikes import SpikeMatrixGroup, spikes2mat
+from assemblyfire.spikes import SpikeMatrixGroup
 from assemblyfire.assemblies import Assembly, AssemblyGroup, ConsensusAssembly
 from assemblyfire.topology import AssemblyTopology
 from assemblyfire.syn_nnd import SynNNDResults
-from assemblyfire.clustering import cluster_sim_mat, cluster_spikes, get_core_cell_idx, detect_assemblies,\
-                                    cluster_assemblies, syn_nearest_neighbour_distances, cluster_synapses
-from assemblyfire import utils
-from assemblyfire import plots
 
