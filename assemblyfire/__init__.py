281471c097511cadea4a1a861c75380920869ba7
Ecker András <andras.ecker@epfl.ch>
2023-06-21 18:15:32 +0200

    config added back

--- assemblyfire/__init__.py
+++ assemblyfire/__init__.py
@@ -1,6 +1,7 @@
 """assemblyfire"""
 
 from assemblyfire.version import __version__
+from assemblyfire.config import Config
 from assemblyfire.spikes import SpikeMatrixGroup
 from assemblyfire.assemblies import Assembly, AssemblyGroup, ConsensusAssembly
 from assemblyfire.topology import AssemblyTopology
